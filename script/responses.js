6084f006bd76d95d5002e292494784ed28b75c11
Jaroslav <profmokytojas@gmail.com>
2022-02-08 14:38:10 +0200

    init commit

---
+++ script/responses.js
@@ -0,0 +1,9 @@
+function getBotResponse(input) {
+  if (input == "Labas") {
+    return "Sveiki!";
+  } else if (input == "Viso gero.") {
+    return "Susisieksime su Jumis!";
+  } else {
+    return "Palikite savo kontaktus ir mes Jums paskambinsime.";
+  }
+}
